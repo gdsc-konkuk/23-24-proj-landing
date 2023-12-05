981b52fb9c8b2e74d6c8e9f584a89f27126985b3
JeongminSeon <skelters@naver.com>
2023-12-06 01:58:19 +0900

    react router
    outlet 구현하여, header, footer 중첩 레이아웃

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -4,14 +4,14 @@ import Header from "../components/Header";
 import Footer from "../components/Footer";
 import RotatingText from "../components/RotatingText";
 import AboutUs from "../components/AboutUs";
+import Slider from "../components/Slider";
 
 export default function Main() {
   return (
     <>
-      <Header />
-      <RotatingText />
-      <AboutUs />
-      <Footer />
+      {/* <RotatingText />
+      <AboutUs /> */}
+      <Slider />
     </>
   );
 }
