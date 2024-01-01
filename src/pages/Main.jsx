431286e78e1689dbb6d37fb24e50c51685e1464e
clice lee <cliceleee@gmail.com>
2024-01-01 15:58:11 +0900

    delete InGdsc.jsx
    problem: infinite loading after yarn start
    
    solution: delete InGdsc.jsx
    delete import, <InGdsc/> in Main.jsx

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -4,7 +4,6 @@ import Header from "../components/Header";
 import Footer from "../components/Footer";
 import RotatingText from "../components/RotatingText";
 import AboutUs from "../components/AboutUs";
-import InGdsc from "../components/InGdsc";
 
 export default function Main() {
   return (
@@ -12,7 +11,6 @@ export default function Main() {
       <Header />
       <RotatingText />
       <AboutUs />
-      <InGdsc/>
       <Footer />
     </>
   );
