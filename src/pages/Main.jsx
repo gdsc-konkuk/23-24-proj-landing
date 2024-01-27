b0131aa448bd0179e191b48b11e1900cdea026b3
JeongminSeon <skelters@naver.com>
2024-01-27 16:02:22 +0900

    video 태그 변경
    video -> gif로 변경하여 img태그 재업로드
    notion page의

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -6,7 +6,6 @@ import InGdsc from "../components/InGdsc";
 import Desktop_blank_frame from "../assets/images/Desktop_blank_frame.png";
 import Video_NotionPage from "../assets/NotionPage/Video_NotionPage.MOV";
 import gdsc_logo from "../assets/images/gdsc_logo.svg";
-import elipse from "../assets/images/elipse.svg";
 
 // InGdsc를 넣으면 localhost:3000 무한 로딩되는 에러가 있음
 // import InGdsc from "../components/InGdsc";
@@ -35,27 +34,24 @@ export default function Main() {
               loop
               autoPlay
               muted
-              className="w-[996px] h-[600px] mt-16 mx-auto "
+              className="w-[996px] h-[600px] mt-16 mx-auto z-[-1]"
               style={{ position: "absolute", top: 40, left: 0 }}
             ></video>
           </div>
         </div>
-        <div className=" mx-auto">
-          <section className="mx-auto">
-            <div className="bg-black w-[100px] h-[100px] rounded-full flex justify-center items-center">
-              <img
-                src={gdsc_logo}
-                alt="gdsc_logo"
-                className=" w-[59px] h-[29px]"
-              />
-            </div>
-          </section>
-          <p>GDSC Konkuk Univ.</p>
-          <p>
-            더 나은 세상을 위한 한 걸음에 보탬이 될 수 있도록 <br />
-            GDSC Konkuk과 함께 해요!
-          </p>
+        <div className="bg-black w-[100px] h-[100px] rounded-full flex justify-center items-center mx-auto">
+          <img src={gdsc_logo} alt="gdsc_logo" className=" w-[59px] h-[29px]" />
         </div>
+
+        <section className="bg-white z-10 w-full flex justify-center">
+          <div className="text-center">
+            <h3 className="font-semibold text-[54px]">GDSC Konkuk Univ.</h3>
+            <p className="text-xl">
+              더 나은 세상을 위한 한 걸음에 보탬이 될 수 있도록 <br />
+              GDSC Konkuk과 함께 해요!
+            </p>
+          </div>
+        </section>
       </div>
     </>
   );
